1e09277165977557e1643fb3ac000fda6d18e3b2
Mohamed El Mahdali <mohamedelmahdali11@gmail.com>
2018-02-27 02:12:21 +0000

    Add solution for Problem 16 in Python

---
+++ Problem 16 - Power digit sum.py
@@ -0,0 +1,9 @@
+"""
+2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2**1000?
+"""
+
+def powerDigitSum(base, power):
+  n = base ** power
+  return sum(int(i) for i in str(n))
