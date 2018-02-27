e26381569d900ba7b35c7b0cdcca1d885373d65a
Mohamed El Mahdali <mohamedelmahdali11@gmail.com>
2018-02-27 01:27:31 +0000

    Fixed mistake

--- Problem 7 - 10001st prime.py
+++ Problem 7 - 10001st prime.py
@@ -3,6 +3,18 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 
 What is the 10 001st prime number?
 """
+from math import sqrt
+
+def getFactors(n):
+  factors = []
+  for i in range(1, int(sqrt(n)) + 1):
+    if n % i == 0:
+      factors.append(i)
+      factors.append(n // i)
+  return factors
+
+def isPrime(n):
+  return len(getFactors(n)) == 2
 
 def nth_prime(n):
   i = 2
